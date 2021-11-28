b44bab16d513060d4bf3683ee41e4e420a154039
Shashikant Yadav <shashikant.sy126@gmail.com>
2021-11-28 15:51:20 +0530

    modernchatbot_website

---
+++ static/scripts/responses.js
@@ -0,0 +1,22 @@
+function getBotResponse(input) {
+   
+    // Simple responses
+    if (input == "hello") {
+        return "hi! This is @ersky how can i help you";
+    } else if (input == "hi") {
+        return "hi! This is @ersky how can i help you";
+    } else if (input == "hi") {
+        return "hi! This is @ersky how can i help you";
+    } else if (input == "tell me somthing about your self") {
+        return "My self shashikant and i have completed my graduation i CSE IN 2021 and i am from azamgarh";
+    } else if (input == "What is your profession") {
+        return "i am node js Developer";
+    } else if (input == "where do you working right now means which company") {
+        return "few months i working on valenta pvt ltd but right now i am looking for node js developer post";
+    } else if (input == "what is your real name") {
+        return "My real name is Shashikant Yadav";
+    
+    } else {
+        return "Thanku to chat with me! have a nice day to you";
+    }
+}
